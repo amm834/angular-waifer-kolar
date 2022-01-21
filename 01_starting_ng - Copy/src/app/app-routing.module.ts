2a0dc61bdf53984ba7a4c04ac68c63d09cfe1f0a
Aung Myat Moe <aungmyatmoe834@gmail.com>
2022-01-21 23:22:38 +0630

    Pass post parameter from HTML template

--- 01_starting_ng - Copy/src/app/app-routing.module.ts
+++ 01_starting_ng - Copy/src/app/app-routing.module.ts
@@ -3,13 +3,11 @@ import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {PostComponent} from "./post/post.component";
-import {PostEditComponent} from "./post-edit/post-edit.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'about', component: AboutComponent},
-  {path: 'post', component: PostComponent},
-  {path: 'post-edit', component: PostEditComponent}
+  {path: 'post/:id', component: PostComponent},
 ];
 
 @NgModule({
